dd2e8258b4de4b44a792bae4e83ac341c1f9ee2d
WonDae <ryunhoo123@kookmin.ac.kr>
2020-02-05 16:14:46 +0900

    게임 수정

--- 0205/백준_1103_게임.cpp
+++ 0205/백준_1103_게임.cpp
@@ -1,105 +1,64 @@
 #include <iostream>
-#include <stack>
+#include <memory.h>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-char map[50][50];
-bool check[50][50][4];
-int v[50][50][4];
+char map[51][51];
+int dp[51][51], n, m;
+bool v[51][51];
+int dx[] = { 0,1,0,-1 };
+int dy[] = { -1,0,1,0 };
+bool infinite;
 
-int dx[] = { -1,0,1,0 };
-int dy[] = { 0,1,0,-1 };
+int dfs(int x, int y)
+{
+	if (infinite) return 0;
+	if (v[x][y])
+	{
+		infinite = true;
+		return 0;
+	}
+	int &ret = dp[x][y];
+	if (ret != -1)
+		return ret;
+	ret = 0;
 
-struct game {
-	int x;
-	int y;
-	int dist;
-	int direction;  // 0은 위, 1은 오른쪽, 2는 아래, 3은 왼쪽
+	int tmp = map[x][y] - 48;
 
-	game(int x, int y, int dist, int direction) :x(x), y(y), dist(dist), direction(direction) {};
-};
+	for (int i = 0; i < 4; i++)
+	{
+		int nx = x + dx[i] * tmp;
+		int ny = y + dy[i] * tmp;
+		if (nx<1 || nx>n || ny<1 || ny>m) continue;
+		if (map[nx][ny] == 'H')
+			continue;
+		v[x][y] = 1;
+		ret = max(ret, dfs(nx, ny) + 1);
+		v[x][y] = 0;
+	}
+
+	return ret;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n, m, ans = 0;
-	cin >> n >> m;
 	string s;
-
-	for (int i = 0; i < n; i++)
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> s;
-		for (int j = 0; j < m; j++)
+		for (int j = 1; j <= m; j++)
 		{
-			map[i][j] = s[j];
-			for (int k = 0; k < 4; k++)
-			{
-				if (map[i][j] == 'H')
-					continue;
-				int tmp = map[i][j] - 48;
-				int nx = i + dx[k] * tmp;
-				int ny = j + dy[k] * tmp;
-				if (nx < 0 || nx >= n || ny < 0 || ny >= m)
-					continue;
-				check[i][j][k] = 1;
-			}
+			map[i][j] = s[j - 1];
 		}
 	}
 
-	stack<game> st;
-	st.push(game(0, 0, 0, 0));
-	v[0][0][0] = 1;
-	v[0][0][1] = 1;
-	v[0][0][2] = 1;
-	v[0][0][3] = 1;
-	
-	bool infinite = false;
-	
-	while (!st.empty())
-	{
-		int x = st.top().x;
-		int y = st.top().y;
-		int d = st.top().dist;
-		int direction = st.top().direction;
-		int tmp = map[x][y] - 48;
-		st.pop();
-
-		printf("nx:%d, ny:%d, direction: %d, v[x][y][direction]: %d\n", x, y, direction,v[x][y][direction]);
-		if (v[x][y][direction] >= 2)
-		{
-			infinite = true;
-			break;
-		}
-
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = x + dx[i] * tmp;
-			int ny = y + dy[i] * tmp;
-			printf("nx:%d, ny:%d\n", nx, ny);
-			if (check[x][y][i])
-			{
-				st.push(game(nx, ny, d + 1,i));
-				v[nx][ny][direction]++;
-			}
-			else
-			{
-				if (map[x][y] == 'H')
-				{
-					if (d > ans)
-						ans = d;
-				}	
-				else
-					if (d + 1 > ans)
-						ans = d + 1;
-			}
-		}
-	}
-
+	memset(dp, -1, sizeof(dp));
+	int tmp = dfs(1, 1);
 	if (infinite)
 		cout << "-1\n";
 	else
-		cout << ans << '\n';
+		cout << tmp + 1 << '\n';
 	return 0;
 }
